b40f196e524bd1da4639821eab65eeccc7e81bd6
migo <migo.vandingo@gmail.com>
2025-02-11 15:07:26 -0700

    adjust response headers

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ logging.basicConfig(filename='record.log',
                      format='%(asctime)s | %(levelname)s | %(lineno)d | \n %(message)-20s')
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True, origins=["http://localhost:5173", "http://localhost:5017"])
 
 #register blueprints
 from api.data_processing_api import data_processing_api
@@ -33,10 +33,11 @@ db = init_db(app)
 @app.before_request
 def handle_options():
     if request.method == 'OPTIONS':
-        response = make_response('success', 200)
-        response.headers['Access-Control-Allow-Headers'] = '*'
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Content-Type'] = '*'
+        response = make_response('', 200)
+        response.headers['Access-Control-Allow-Origin'] = "http://localhost:5173"
+        response.headers['Access-Control-Allow-Methods'] = "POST, GET, OPTIONS, DELETE, PUT"
+        response.headers['Access-Control-Allow-Headers'] = "Content-Type, Authorization"
+        response.headers['Access-Control-Allow-Credentials'] = "true"
         return response
     else:
         request_id = str(uuid.uuid4())
